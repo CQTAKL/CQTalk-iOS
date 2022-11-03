d2e19d3ba3e935f091bcee2ea3b3c53dbb1c8c7f
Haren <human01d@icloud.com>
2022-11-03 22:46:17 +0800

    Initial Commit

---
+++ CQTalk/CQTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CQTalkApp.swift
+//  CQTalk
+//
+//  Created by 陈驰坤 on 2022/11/3.
+//
+
+import SwiftUI
+
+@main
+struct CQTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
