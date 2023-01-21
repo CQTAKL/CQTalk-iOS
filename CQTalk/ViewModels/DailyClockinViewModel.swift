31280e39263c97c25e8b8884244d2342504be082
haren724 <haren724@icloud.com>
2023-01-21 20:59:33 +0800

    创建每日打卡的viewModel，准备更换数据管理方式

---
+++ CQTalk/ViewModels/DailyClockinViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  DailyClockinViewModel.swift
+//  CQTalk
+//
+//  Created by 陈驰坤 on 2023/1/21.
+//
+
+import SwiftUI
+
+extension DailyClockInView {
+    @MainActor final class DailyClockinViewModel: ObservableObject {
+        
+    }
+}
