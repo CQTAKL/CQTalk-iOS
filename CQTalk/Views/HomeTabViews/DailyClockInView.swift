15bbd7220565e21af150ef76fc0b83b53a2dc4f5
haren724 <haren724@icloud.com>
2023-01-22 00:17:07 +0800

    每日打卡的大致雏形已建成，正在解决顶部按钮过多导致的布局问题

--- CQTalk/Views/HomeTabViews/DailyClockInView.swift
+++ CQTalk/Views/HomeTabViews/DailyClockInView.swift
@@ -4,11 +4,59 @@ struct DailyClockInView: View {
     
     @State private var str: String = ""
     @State private var str2: String = ""
+    @State private var selectedDate: Date = Date.now
+    @State private var subjects: [Subject] = [.higherMath, .linearAlgebra]
+    
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
+    enum Subject: String {
+        case higherMath = "高等数学",
+             linearAlgebra = "线性代数"
+    }
+    
+    enum Quiz {
+        case tdy, yst1, yst2, etc
+    }
+    
 //  单个题目的提交面板
-    func submitPanel() -> some View {
-        Text("<#Hello, world!#>")
+    struct SubmitPanel: View {
+        @Binding var quizName: String
+        @Binding var answer: String
+        
+//        init(quizName: Binding<String>, answer: Binding<String>) {
+//            self._quizName = quizName
+//            self._answer = answer
+//        }
+        
+        var body: some View {
+            VStack {
+                VStack {
+                    HStack {
+                        Text("题目\(1)：")
+                        Spacer()
+                    }
+                    Divider()
+                    VStack {
+                        Group {
+                            TextField("题目", text: $quizName)
+                            TextEditor(text: $answer)
+                                .frame(height: 200)
+                                .border(.secondary)
+                                .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                        }.textFieldStyle(.roundedBorder)
+                        HStack(spacing: 20) {
+                            Button("得分说明") { }
+                            Button("查看往期打卡项目") { }
+                            Button("明日出题说明") { }
+                            Spacer()
+                            Button("提交") { }.buttonStyle(.borderedProminent)
+                        }.padding(.vertical).font(.footnote)
+                    }
+                }.padding()
+            }.background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                .padding()
+        }
     }
 
 //  今日题目、昨日题目、前日题目、往常题目（这四个页面TabView的导航栏）
@@ -17,68 +65,50 @@ struct DailyClockInView: View {
     }
     
     var body: some View {
-        ScrollView {
-            HStack {
-                if horizontalSizeClass == .regular {
-                    Spacer()
-                }
+        HStack {
+            if horizontalSizeClass == .regular {
+                Spacer()
+            }
+            VStack {
+                ScrollView(.horizontal) {
+                    HStack {
+                        Button("打卡记录") { }
+                        Button("补签") { }
+                        Button("我的信息") { }
+                        Button("排行榜") { }
+                        Button("提供建议") { }
+                        Button("打卡规则") { }
+                    }.font(.footnote)
+                        .padding(.top)
+                        .buttonStyle(.borderedProminent)
+                }.scrollIndicators(.hidden)
+                HStack(spacing: 20) {
+                    Button("今日题目") { }
+                    Button("昨日题目") { }
+                    Button("前日题目") { }
+                    Button("往常题目") { }
+                }.padding()
                 VStack {
                     VStack {
-                        VStack {
-                            HStack {
-                                Text("我的打卡")
-                                Text("补签")
-                                Spacer()
-                                Button("打卡规则") { }
-                            }
-                            Divider()
-                            VStack {
-                                HStack {
-                                    Text("今日打卡题目")
-                                    Spacer()
-                                    Text("科目：")
-                                    Text("高数")
-                                    Button("切换") { }
-                                    Spacer()
-                                }
-                                HStack {
-                                    Text("2022年10月18日")
-                                    Spacer()
-                                    Text("目前参与人数：")
-                                    Text("10")
-                                }
-                                Divider()
-                                Group {
-                                    TextField("题目", text: $str)
-                                    TextEditor(text: $str2)
-                                        .frame(height: 200)
-                                        .border(.secondary)
-                                        .clipShape(RoundedRectangle(cornerRadius: 4.0))
-                                }.textFieldStyle(.roundedBorder)
-                                HStack {
-                                    Spacer()
-                                    Button {} label: {
-                                        HStack {
-                                            Image(systemName: "icloud.and.arrow.up")
-                                            Text("提交")
-                                        }.foregroundColor(.white)
-                                            .padding(4)
-                                    }.background(Color.accentColor)
-                                        .clipShape(RoundedRectangle(cornerRadius: 8.0))
-                                }.padding(.vertical)
-                            }
-                        }.padding()
                         HStack {
-                            Button("查看往期打卡项目") {}.font(.footnote)
+                            Text("日期：\(selectedDate.formatted(date: .abbreviated, time: .omitted))").environment(\.locale, Locale(identifier: "zh_CN"))
                             Spacer()
-                        }.padding([.leading, .bottom])
-                    }.background(.ultraThinMaterial)
-                        .clipShape(RoundedRectangle(cornerRadius: 8.0))
-                        .padding()
-                }.frame(maxWidth: 800)
-                if horizontalSizeClass == .regular {
-                    Spacer()
+                        }
+                        Divider().padding(.horizontal)
+                        HStack {
+                            Text("科目选择：\(subjects.map({ $0.rawValue }).joined(separator: "、"))")
+                            Spacer()
+                            Button("选择") { }.font(.footnote).buttonStyle(.borderedProminent)
+                        }
+                    }.padding()
+                }.background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                ScrollView {
+                    SubmitPanel(quizName: $str, answer: $str2)
                 }
+            }.frame(maxWidth: 800)
+            if horizontalSizeClass == .regular {
+                Spacer()
             }
         }
     }
@@ -87,5 +117,6 @@ struct DailyClockInView: View {
 struct DailyClockInView_Previews: PreviewProvider {
     static var previews: some View {
         DailyClockInView()
+        HomePageView()
     }
 }
